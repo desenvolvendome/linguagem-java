e7b58b3199d833c2a927c44993d210128c0e77a5
Arlisson <Arlissonjaques@gmail.com>
2020-09-30 10:30:00 -0300

    Added menu

--- Arlisson/Java/src/Main.java
+++ Arlisson/Java/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Seja bem vindo! \nEscolha o numero do algoritmo que deseja esperimentar: \n");
+
+        int option = -1;
+
+        while (option < 0 || option > 1){
+
+            System.out.println("1) - Factorial");
+            System.out.println("2) - Linear Search");
+            System.out.println("1) - Bubble Sort");
+            System.out.println("0) - Sair");
+
+            System.out.print(">>> ");
+            option = input.nextInt();
+
+            if(option < 0 || option > 1)
+                System.out.println("Opss, opção invalida!");
+        }
+
 
     }
 }
