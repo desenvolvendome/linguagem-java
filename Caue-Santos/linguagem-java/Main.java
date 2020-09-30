5a1594a8315bd32471dde1787da6d7befa22ddc8
Cauê Santos <caah.sfc@live.com>
2020-09-30 10:58:49 +0000

    Criando o algoritmo para calcular o fatorial e o chamando na classe principal

--- Caue-Santos/linguagem-java/Main.java
+++ Caue-Santos/linguagem-java/Main.java
@@ -2,5 +2,8 @@ public class Main {
   public static void main(String[] args) {  
     BubbleSort bubbleSort = new BubbleSort();
     bubbleSort.calcula();
+
+    Fatorial fatorial = new Fatorial();
+    fatorial.calcula();
   }
 }
