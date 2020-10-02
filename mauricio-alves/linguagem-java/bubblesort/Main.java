16794da29364248caad29c95a799af661daf83f6
repl.it user <replituser@example.com>
2020-10-02 23:28:47 +0000

    Atualizando Atividades

---
+++ mauricio-alves/linguagem-java/bubblesort/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int[]v = {55,35,45,70,24,12,14,28,99};
+		BubbleSort.bolha(v);
+		System.out.println("Array Ordenado: "+Arrays.toString(v));
+
+	}
+
+}
